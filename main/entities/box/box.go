e81145005bb0e840d3f5650bf635f04f9724f26b
Lucas Blotta <lucas@blotta.info>
2026-02-04 11:28:31 -0300

    copter base mechanics

---
+++ main/entities/box/box.go
@@ -0,0 +1,43 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"box\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/entities/box/box.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    y: 16.0
+  }
+}
+embedded_components {
+  id: "co"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 1.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"box\"\n"
+  "mask: \"floor\"\n"
+  "mask: \"copter\"\n"
+  "mask: \"box\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      y: 16.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "    id: \"box_shape\"\n"
+  "  }\n"
+  "  data: 16.0\n"
+  "  data: 16.0\n"
+  "  data: 10.0\n"
+  "}\n"
+  ""
+}
---
+++ main/entities/copter/copter.go
@@ -0,0 +1,56 @@
+components {
+  id: "copter"
+  component: "/main/entities/copter/copter.script"
+}
+components {
+  id: "player"
+  component: "/main/entities/copter/player.script"
+}
+components {
+  id: "hook"
+  component: "/main/entities/copter/hook.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"copter\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/entities/copter/copter.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    y: 36.0
+  }
+}
+embedded_components {
+  id: "co"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 5.0\n"
+  "friction: 0.9\n"
+  "restitution: 0.2\n"
+  "group: \"copter\"\n"
+  "mask: \"box\"\n"
+  "mask: \"floor\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      y: 20.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "    id: \"box\"\n"
+  "  }\n"
+  "  data: 20.0\n"
+  "  data: 20.0\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.5\n"
+  "angular_damping: 0.9\n"
+  ""
+}
