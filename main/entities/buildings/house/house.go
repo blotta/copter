03b3f6c3ec4e1dc487eb7b0302c734c97bce8db8
Lucas Blotta <lucas@blotta.info>
2026-02-12 12:01:51 -0300

    added house building

---
+++ main/entities/buildings/house/house.go
@@ -0,0 +1,14 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"building-house\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/entities/buildings/house/house.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    z: -0.1
+  }
+}
